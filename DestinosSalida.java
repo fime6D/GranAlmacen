fbd2254256aa503d6329b44b92effb0d04665e97
fime6d <avalosjoseph@gmail.com>
2013-04-25 02:27:28 -0500

    estas son las versiones ya terminadas
    una es la de los diagramas que a mi me tocaron ATT ALFONSO, y las
    relaciones adaptadas con la ultima tabla

---
+++ DestinosSalida.java
@@ -0,0 +1,25 @@
+
+
+/**
+ * @author Poncho
+ * @version 1.0
+ * @created 20-mar-2013 11:26:48 p.m.
+ */
+public class DestinosSalida {
+
+	private char departamento;
+	private int idDestino;
+
+	public DestinosSalida(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+
+	public void agrega(){
+
+	}
+
+}
